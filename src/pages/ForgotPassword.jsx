3fc3abfd1839718b18976a04c172666596f48652
happy <happyraj9939@gmail.com>
2024-03-29 13:52:43 +0530

    complete forgot password page functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,12 +2,25 @@ import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 export default function ForgotPassword() {
-  
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState("");
 
   function onChange(e) {
     setEmail(e.target.value);
+  }
+  async function onSubmit(e){
+         e.preventDefault()
+         try {
+          const auth=getAuth();
+          await sendPasswordResetEmail(auth,email);
+          toast.success("Email was sent");
+          
+         } catch (error) {
+          toast.error("Could not send reset password")
+  
+         }
   }
 
   return (
@@ -22,7 +35,7 @@ export default function ForgotPassword() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
@@ -32,37 +45,39 @@ export default function ForgotPassword() {
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
 
-           
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg ">
+              <p className="mb-6 ">
+                Don't have a account?
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/sign-in"
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out "
+                >
+                  Sign in instead
+                </Link>
+              </p>
+            </div>
 
-             
-           <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg ">
-           <p className="mb-6 ">
-              Don't have a account?
-              <Link to='/sign-up' className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1">Register</Link>
-             </p>
-             <p>
-             <Link to='/sign-in' className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out ">Sign in instead</Link>
-             </p>
-           </div>
+            <button
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 "
+              type="submit"
+            >
+              Set reset password
+            </button>
 
+            <div className="my-4  items-center flex before:border-t before:flex-1  before:border-gray-300   after:border-t after:flex-1  after:border-gray-300">
+              <p className="text-center font-semibold mx-4 ">OR</p>
+            </div>
 
-
-
-
-    
-
-          <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 "   type="submit" >Set reset password</button>
-
-          <div className="my-4  items-center flex before:border-t before:flex-1  before:border-gray-300   after:border-t after:flex-1  after:border-gray-300">
-            <p className="text-center font-semibold mx-4 ">OR</p>
-          </div>
-
-          <OAuth/>
-
-
+            <OAuth />
           </form>
-          
-
         </div>
       </div>
     </section>
