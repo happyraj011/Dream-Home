29ee6e17b1d14718d22d8357745241e54423f9dc
happy <happyraj9939@gmail.com>
2024-03-29 13:25:53 +0530

    complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { toast } from "react-toastify";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+
+
+
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
@@ -10,12 +16,30 @@ export default function SignIn() {
     password: "",
   });
   const { email, password } = formData;
+  const navigate=useNavigate();
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
   }
+
+
+  async function onSubmit(e){
+    e.preventDefault();
+    try {
+      const auth=getAuth();
+      const userCredential=await signInWithEmailAndPassword(auth,email,password);
+      if(userCredential.user){
+        navigate("/")
+      }
+
+      
+    } catch (error) {
+      toast.error("Bad user credentials")
+    }
+  }
+
 
   return (
     <section>
@@ -29,7 +53,7 @@ export default function SignIn() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
